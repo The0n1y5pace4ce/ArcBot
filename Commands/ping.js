11eb8ebc5df2ecb5f6ef66cfe0e5c51d410c65cc
The0n1y5pace4ce <spaceacethe625@gmail.com>
2022-04-28 14:11:54 +1000

    dont need that anymore

--- Commands/ping.js
+++ Commands/ping.js
@@ -10,18 +10,6 @@ module.exports = {
     aliases: ['p'],
     slash: 'both',
     callback: ({interaction, message}) => {
-        let storedSettings = await GuildSettings.findOne({
-            guildID: message.guild.id || interaction.guild.id,
-          });
-          if (!storedSettings) {
-            const newSettings = new GuildSettings({
-              guildID: message.guild.id,
-            });
-            await newSettings.save().catch((e) => {
-              console.log(e);
-            });
-            storedSettings = await GuildSettings.findOne({ guildID: message.guild.id });
-          }
 
           return `Pong! ${client.ws.ping}`
     }
